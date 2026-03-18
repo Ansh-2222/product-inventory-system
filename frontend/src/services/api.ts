98a0a267518678ef96026deeb2d97c6407fbf648
Ansh <sharmansh12@gmail.com>
2026-03-18 12:01:36 +0530

    api.ts in frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,13 +1,21 @@
 // src/services/api.ts
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL}/api`
+  : "http://localhost:5000/api";
+
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: BASE_URL,
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
   return config;
 });
 
